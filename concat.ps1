0b5f487b617b516bfecaaaf6360297cfdd2ccdd9
nrv30 <nrv303511@gmail.com>
2025-05-31 23:37:52 -0700

    Update concat.ps1

--- concat.ps1
+++ concat.ps1
@@ -1,5 +1,6 @@
 # takes path to textfile which contains the files to be concatenated
 # follow theses instructions to make your textfile <<https://trac.ffmpeg.org/wiki/Concatenate#demuxer>>
+# or use script `build_concatDemux_file.ps1` to build it based for a directory of vids
 
 param($inputFile, $outputFile)
 ffmpeg -f concat -safe 0 -i $inputFile -c:v libx264 -preset fast -crf 23 $outputFile
