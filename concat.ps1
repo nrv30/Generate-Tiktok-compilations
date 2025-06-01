76723a0d729ccfd3f4962b466ee7c85311e52291
nrv30 <nrv303511@gmail.com>
2025-05-31 18:37:51 -0700

    Update concat.ps1

--- concat.ps1
+++ concat.ps1
@@ -1,2 +1,5 @@
+# takes path to textfile which contains the files to be concatenated
+# follow theses instructions to make your textfile <<https://trac.ffmpeg.org/wiki/Concatenate#demuxer>>
+
 param($inputFile, $outputFile)
 ffmpeg -f concat -safe 0 -i $inputFile -c:v libx264 -preset fast -crf 23 $outputFile
