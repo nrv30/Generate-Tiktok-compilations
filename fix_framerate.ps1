ab53548db3cc504047de7505fbff4f7e7cf5422e
nrv30 <nrv303511@gmail.com>
2025-05-31 23:56:37 -0700

    Update fix_framerate.ps1

--- fix_framerate.ps1
+++ fix_framerate.ps1
@@ -1,4 +1,5 @@
 # change framerate of an input file
 
 param ($input_file, $fps, $output_file)
-ffmpeg -i $input_file -vf "fps=`"$fps`"" -c:v libx264 -preset fast -crf 23 $output_file
+$fps = 30
+Start-Process -FilePath "ffmpeg.exe" -ArgumentList "-i `"$input_file`"", "-vf `"fps=$fps`"", "-c:v libx264 -preset fast -crf 23", "`"$output_file`"" -NoNewWindow -Wait
