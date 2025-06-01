2174698bd4753ad4a4047d451c11ee2ff4d7fd6e
nrv30 <nrv303511@gmail.com>
2025-05-31 17:57:42 -0700

    Update hstackSimp.ps1

--- hstackSimp.ps1
+++ hstackSimp.ps1
@@ -1,10 +1,2 @@
 param($paths1, $paths2, $paths3, $name)
-
-# & ffmpeg -i $paths1 -r 30 -c:v libx264 -preset fast -crf 23 $name
-# & ffmpeg -i $paths2 -r 30 -c:v libx264 -preset fast -crf 23 ("2" + $name) 
-# & ffmpeg -i $paths3 -r 30 -c:v libx264 -preset fast -crf 23 ("3" + $name)
-
 & ffmpeg -i $paths1 -i $paths2 -i $paths3 -filter_complex "[0:v][1:v][2:v]hstack=inputs=3:shortest=1" -shortest $name
-
-# timestamp
-# 00:40:00
